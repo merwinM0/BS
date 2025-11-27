f591ca63eec677d68e7adc79adbfe3f42953b50d
merwinM0 <18358511710@139.com>
2025-11-27 14:50:18 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from bs!")
+import pandas as pd
 
+# 读取CSV文件
+df = pd.read_csv('../NBA-Data-2010-2024-main/play_off_totals_2010_2024.csv')
 
-if __name__ == "__main__":
-    main()
+# 显示数据行数
+print(f"行数: {len(df)}")
